1d6518dbcbbb4a0fa4c7768e678d69ddf8bc3e64
halalh4x0r <mohamed.hadi@student.moringaschool.com>
2025-11-15 11:54:07 +0300

    modified GitRequester.py

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,16 @@
+# lib/GetRequester.py
+
 import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
-
+    
     def get_response_body(self):
-        pass
-
+        response = requests.get(self.url)
+        return response.content
+    
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
